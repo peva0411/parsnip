d160813f99eb7c27fa5ef2b6161f3421dbdf420d
Phil Evans <pevans@paylocity.com>
2018-07-14 15:12:55 -0400

    removing extra reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -8,23 +8,20 @@ export default function tasks(state = {tasks:[]}, action){
         tasks : action.payload.tasks,
       };
     }
-    case 'EDIT_TASK':{
-      const {payload} = action;
-      return {
-        tasks: state.tasks.map(task => {
-          if (task.id === payload.id){
-            return Object.assign({}, task, payload.params);
-          }
-          return task;
-        })
-      };
-    }
-    case 'CREATE_TASK':{
-      return  {tasks: state.tasks.concat(action.payload)};
-    }
     case 'CREATE_TASK_SUCCEEDED':{
       return {
         tasks: state.tasks.concat(action.payload.task)
+      };
+    }
+    case 'EDIT_TASK_SUCCEEDED':{
+      const {payload} = action;
+      return {
+        tasks:state.tasks.map(task => {
+          if (task.id === payload.task.id){
+            return payload.task;
+          }
+          return task;
+        }),
       };
     }
     default: {
